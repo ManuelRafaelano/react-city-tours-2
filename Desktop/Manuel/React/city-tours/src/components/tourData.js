35b3570de182ef274ee6accd9e4c1e2bf22db05d
ManuelRafaelano <manuelrafaelano@gmail.com>
2019-03-04 13:40:31 -0600

    first commit

---
+++ Desktop/Manuel/React/city-tours/src/components/tourData.js
@@ -0,0 +1,44 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 5,
+    city: "Magika No Kenshi To Shoukan Maou",
+    img:
+      "https://4.bp.blogspot.com/-ytfFWDbkz7g/WbBp4lcDXrI/AAAAAAAALg0/_AFKKVLVGLIW71ho02FzbZU6ywLI3nPSACLcBGAs/s640/Ilustraciones-de-Magika-No-Kenshi-To-Shoukan-Maou-Volumen-14-2.jpg",
+    name:
+      "se convirtió en un verdadero Rey del Harem, con más de 10 chicas y varias embarazadas",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  }
+];
